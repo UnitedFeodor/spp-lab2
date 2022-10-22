e0c5f9cf1c0ee12e63ca6209fc191444dd523aa4
UnitedFeodor <86407851+UnitedFeodor@users.noreply.github.com>
2022-10-22 21:18:09 +0300

    some abstractions and stuff

---
+++ spp-lab2/IFaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spp_lab2
+{
+    public interface IFaker
+    {
+        T Create<T>();
+        object Create(Type type);
+    }
+}
