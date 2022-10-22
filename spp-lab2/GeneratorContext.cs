b572200ccb3ed442d59116f55cff93dc6426f662
UnitedFeodor <86407851+UnitedFeodor@users.noreply.github.com>
2022-10-22 21:35:32 +0300

    'ignore'

--- spp-lab2/GeneratorContext.cs
+++ spp-lab2/GeneratorContext.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace spp_lab2
 {
-    internal class GeneratorContext
+    public class GeneratorContext
     {
+        // Для сохранения состояния генератора псевдослучайных чисел
+        // и получения различных значений при нескольких последовательных вызовах.
+        public Random Random { get; }
+
+        // Даем возможность генератору использовать все возможности Faker.
+        // Необходимо для создания коллекций произвольных объектов,
+        // но может быть удобно и в некоторых других случаях.
+        public Faker Faker { get; }
+
+        public GeneratorContext(Random random, IFaker faker)
+        {
+            Random = random;
+            Faker = faker;
+        }
     }
 }
